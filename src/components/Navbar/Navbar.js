5d80b083d94d1c348e34dfed5e1246b313027a3d
seattle-mini <sl8305@hotmail.com>
2020-04-27 20:49:08 -0700

    boiler plate with test case of using icons fron Ant Design

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import GithubOutlined from '@ant-design/icons';
+
+function Navbar() {
+    return (
+
+        <div>   
+            <p> Hello World </p>
+            <GithubOutlined />
+            
+        </div>
+
+        
+
+    );
+
+
+    
+}
+
+export default Navbar;
